fb304c1d5a3b1264fc048c6924b74f663fff4f12
Erkka Mutanen <erkka.mutanen@sievo.com>
2019-02-04 10:26:31 +0200

    Change ButtonGroup text.

--- src/Components/ButtonGroup.jsx
+++ src/Components/ButtonGroup.jsx
@@ -8,7 +8,8 @@ const MarketIndexButtonGroup = () => (
     <h1 className="headline">Test by clicking the button and selecting a time range.</h1>
     <div className="instruction" style={{ width: "600px" }}>
       <h2>* It is possible to select a starting point.</h2>
-      <h2>* It is possible to click arrow button to go back a few years.</h2>
+      <h2>* It is possible to click arrow button to go back max 10 years.</h2>
+      <h2>* Upon opening, the picker will always open from the current year.</h2>
       <h2>* The selection can be applied until the current month that we live in.</h2>
       <h2>* There is no future time selection support.</h2>
     </div>
